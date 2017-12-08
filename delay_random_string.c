e814b1031d3ae196164ab75248ce1b68493ff6dc
Timothy Sullivan <tsullivan@elastic.co>
2017-12-07 22:46:35 -0700

    simplify

--- delay_random_string.c
+++ delay_random_string.c
@@ -4,34 +4,20 @@
 #include <unistd.h>
 #include <time.h>
 
-static char *rand_string_generator(char *str, size_t size)
+static char *rand_string(char *str, size_t size)
 {
   const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJK...";
-  if (size) {
-    --size;
-    for (size_t n = 0; n < size; n++) {
-      int key = rand() % (int) (sizeof charset - 1);
-      str[n] = charset[key];
-    }
-    str[size] = '\0';
+  for (size_t n = 0; n <= size; n++) {
+    int key = rand() % (int) (sizeof charset - 1);
+    str[n] = charset[key];
   }
+  str[size] = '\0';
   return str;
-}
-
-void rand_string(int prefix, size_t size)
-{
-  char *s = malloc(size + 1);
-  if (s) {
-    rand_string_generator(s, size);
-  }
-  fprintf(stdout, "%d-%s\n", prefix, s);
-  free(s);
 }
 
 int main(int argc, char**argv)
 {
   useconds_t mult_factor=0, delay;
-  int str_size;
   if (argc<2){
     fprintf(stderr,"%s <delay factor (in milli-seconds)>:\n\techo different random strings infinitely after a pre-set random delay\n",argv[0]);
     exit(1);
@@ -42,8 +28,13 @@ int main(int argc, char**argv)
   delay = (rand() / (1.0 + RAND_MAX)) * mult_factor * 1000;
 
   while(1) {
-    str_size = (rand() / (1.0 + RAND_MAX)) * mult_factor;
-    rand_string(delay, str_size);
+    int str_size = (rand() / (1.0 + RAND_MAX)) * mult_factor;
+    /* allocate string */
+    char *s = malloc(str_size + 1);
+    if (s && str_size) {
+      fprintf(stdout, "%s\n", rand_string(s, str_size));
+    }
+    free(s);
     usleep(delay);
   }
 }
